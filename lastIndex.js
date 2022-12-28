3b8393c661a2cda5202d4352d59cce1bfa063f31
andrii-grynik <andrii.grynik@gmail.com>
2022-12-28 14:29:57 -0700

    Looping over indexs

---
+++ lastIndex.js
@@ -0,0 +1,12 @@
+const lastIndexOf = (a, b) => {
+  let index = -1;
+  a.forEach(function(element, i) {
+    if (element === b) index = i;
+  });
+  return index;
+}
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
